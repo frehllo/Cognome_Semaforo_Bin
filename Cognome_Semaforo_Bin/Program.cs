7e06b1e54408b9142c6a673a50bf9663c893f991
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2021-05-07 08:59:03 +0200

    Add project files.

---
+++ Cognome_Semaforo_Bin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cognome_Semaforo_Bin
+{
+    class Program
+    {
+        static int n;
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                n = 0;
+                Thread t1 = new Thread(() => Incrementa());
+                Thread t2 = new Thread(() => Decrementa());
+                t1.Start();
+                t2.Start();
+                while(t1.IsAlive) { }
+                while(t2.IsAlive) { }
+                Console.WriteLine($"{n}");
+                Console.ReadLine();
+            }
+        }
+
+        public static void Incrementa()
+        {
+            for(int i = 0; i <= 1000000; i++)
+            {
+                n++; 
+            }
+        }
+
+        public static void Decrementa()
+        {
+            for (int i = 0; i <= 1000000; i++)
+            {
+                n--;
+            }
+        }
+    }
+}
